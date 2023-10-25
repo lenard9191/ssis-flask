0f0f893d43c9e59c33a00a7f8c9710199b66e65f
John Lenard Requina <lenard147896325@gmail.com>
2023-10-25 08:52:54 +0800

    added student edit functionality

--- ssis/routes/student.py
+++ ssis/routes/student.py
@@ -21,7 +21,7 @@ def student():
     return render_template('student_home.html', colleges=colleges , courses=courses,students=students)
 
 
-@student_bp.route("/student/add", methods=['POSt'])
+@student_bp.route("/student/add", methods=['POST'])
 def student_add():
     id = request.form.get('id')
     firstname = request.form.get('firstname')
@@ -51,16 +51,54 @@ def student_add():
             })
     
 
-@student_bp.route("/student/delete", methods=['GET', 'POST'])
+@student_bp.route("/student/delete", methods=['POST'])
 def student_delete():
     try:
         id = request.form.get('csasdsda')
         student = Student.get_one(id)
         student.delete()
         return redirect(url_for("student_bp.student"))
-
     except Exception as e:
         error = f"Error: {e}"
         return jsonify({
             'error' : error
         })
+
+
+@student_bp.route("/student/edit", methods=['POST'])
+def student_edit():
+    pastid = request.form.get('pastid')
+    id = request.form.get('id')
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    course_code = request.form.get('course_code')
+    year = request.form.get('year')
+    gender = request.form.get('gender')
+    
+    
+    student = Student.get_one(pastid)
+    if id != pastid:
+        exist_student = Student.get_one(id)
+        if exist_student:
+            error = f"Student ID: {id} is already taken"
+            return jsonify({'error' : error})
+        else:
+            try:
+                student = Student(id=id,firstname=firstname,lastname=lastname,course_code=course_code,year=year,gender=gender)
+                student.update(pastid)
+                return redirect(url_for("student_bp.student"))
+            except Exception as e:
+                return jsonify({'error': e})
+    else:
+        try:
+            student.firstname = firstname
+            student.lastname = lastname
+            student.course_code = course_code
+            student.year = year
+            student.gender = gender
+            student.update(id)
+            return redirect(url_for("student_bp.student"))
+        except Exception as e:
+            return jsonify({'error' : e})
+        
+
--- ssis/static/student.js
+++ ssis/static/student.js
@@ -1,53 +1,86 @@
 $(document).ready(function () {
+  $("#addStudentForm").on("submit", function (event) {
+    $.ajax({
+      type: "POST",
+      url: "/student/add",
+      data: {
+        id: $("#student_id").val(),
+        firstname: $("#student_first_name").val(),
+        lastname: $("#student_last_name").val(),
+        course_code: $("#student_course_code").val(),
+        year: $("#student_year").val(),
+        gender: $("#student_gender").val(),
+      },
+    }).done(function (data) {
+      if (data.error) {
+        $("#erroraddstdmsg").text(data.error).show();
+      } else {
+        alert("Successfully added student");
+        window.location.href = "/student";
+      }
+    });
+    event.preventDefault();
+  });
 
-    $("#addStudentForm").on("submit", function (event) {  
+  $(".delete-Student").click(function () {
+    var studentId = $(this).data("student-id");
+
+    $("#deleteStudentForm").click(function () {
+      $.ajax({
+        type: "POST",
+        url: "/student/delete",
+        data: { csasdsda: studentId },
+      }).done(function (data) {
+        if (data.error) {
+          $("#errordeletstdemsg").text(data.error).show();
+        } else {
+          alert("Sucessfully deleted student: " + studentId);
+          window.location.href = "/student";
+        }
+      });
+    });
+  });
+
+  $(".edit-Student").on("click", function () {
+    var id = $(this).data("student-id");
+    var firstname = $(this).data("student-firstname");
+    var lastname = $(this).data("student-lastname");
+    var course_code = $(this).data("student-course");
+    var year = $(this).data("student-year");
+    var gender = $(this).data("student-gender");
+
+    $("#edit_student_id").val(id);
+    $("#edit_student_first_name").val(firstname);
+    $("#edit_student_last_name").val(lastname);
+    $("#edit_student_course_code").val(course_code);
+    $("#edit_student_year").val(year);
+    $("#edit_student_gender").val(gender);
+
+    $('#editStudentForm').on("submit", function (event) {
         $.ajax({
             type : "POST",
-            url : "/student/add",
-            data : {
-                id : $("#student_id").val(),
-                firstname: $("#student_first_name").val(),
-                lastname: $("#student_last_name").val(),
-                course_code: $("#student_course_code").val(),
-                year: $("#student_year").val(),
-                gender : $("#student_gender").val(),
+            url : "/student/edit",
+            data: {
+                pastid : id,
+                id: $("#edit_student_id").val(),
+                firstname: $("#edit_student_first_name").val(),
+                lastname: $("#edit_student_last_name").val(),
+                course_code: $("#edit_student_course_code").val(),
+                year: $("#edit_student_year").val(),
+                gender: $("#edit_student_gender").val()
+
             }
-        }).done (function (data) {
+
+        }).done(function (data) {
             if (data.error) {
-                $("#erroraddstdmsg").text(data.error).show()
+                $("#erroreditstdmsg").text(data.error).show();
             }
             else {
-                alert("Successfully added student")
+                alert("Successfully edited student")
                 window.location.href = "/student"
             }
         });
-        event.preventDefault();
+        event.preventDefault()
     });
-
-    $(".delete-Student").click(function () {
-        var studentId = $(this).data("student-id");
-    
-
-        $("#deleteStudentForm").click(function () {
-            $.ajax({
-                type: "POST",
-                url: "/student/delete",
-                data: { csasdsda: studentId }
-            }).done(function (data) {
-                if (data.error) {
-                    $("#errordeletstdemsg").text(data.error).show();
-                }
-                else {
-                    alert("Sucessfully deleted student: " + studentId)
-                    window.location.href = "/student"
-    
-                }
-            });    
-        });
-
-
-      });
-
-
-
+  });
 });
